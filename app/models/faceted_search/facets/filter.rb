819440cd2c0e76c5479c6685b3825d929e74b4b1
Sébastien Gaya <sebastien.gaya@gmail.com>
2019-03-13 17:53:42 +0100

    1.1.2 : results count

--- app/models/faceted_search/facets/filter.rb
+++ app/models/faceted_search/facets/filter.rb
@@ -12,17 +12,34 @@ module FacetedSearch
       value.to_s.in? params_array
     end
 
+    def results_with(value)
+      scope = @facets.model
+      @facets.list.each do |facet|
+        scope = facet == self ? add_scope_with_value(scope, value)
+                              : facet.add_scope(scope)
+      end
+      scope
+    end
+
     def params_array
       @params_array ||= @params.to_s.split(',')
     end
 
     # Adds a scope corresponding to this facet
     # to the scope sent as an argument
-    # and return the modified scope 
+    # and return the modified scope
     def add_scope(scope)
       return scope if params_array.blank?
       @habtm  ? scope.joins(name).where(name => { find_by => params_array })
               : scope.where(name => params_array)
+    end
+
+    # Adds a scope corresponding to this facet
+    # to the scope sent as an argument with specific value
+    # and return the modified scope
+    def add_scope_with_value(scope, value)
+      @habtm  ? scope.joins(name).where(name => { find_by => value })
+              : scope.where(name => value)
     end
 
     # Show all values that have corresponding results.
