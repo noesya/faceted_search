37d639a4af7869d49c6118372699e7782554c7ad
Arnaud Levy <alevy@lespoupees.paris>
2019-04-01 18:01:18 +0200

    children scope

--- app/models/faceted_search/facets/tree.rb
+++ app/models/faceted_search/facets/tree.rb
@@ -1,22 +1,34 @@
 module FacetedSearch
   class Facets::Tree < Facets::DefaultList
 
+    def children_scope
+      @options[:children_scope] ||= Proc.new { |children| children }
+    end
+
     def selected_objects
       source.where(find_by => params_array)
     end
 
     def selected_object
-      selected_objects.last
+      source.find_by(find_by => params_array.last)
     end
 
     # All values, not filtered
     # Otherwise, we would need to search all the children to do a good filtering
     def values
-      root? ? source.root
-            : selected_object.children
+      root? ? values_root
+            : values_children
     end
 
     protected
+
+    def values_root
+      source.root
+    end
+
+    def values_children
+      children_scope.call selected_object.children
+    end
 
     def root?
       params_array.empty?
