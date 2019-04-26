6f03371cb0ba05b725991c7523e4f2fa42b64795
Arnaud Levy <alevy@lespoupees.paris>
2019-04-26 17:26:43 +0200

    filter full tree

--- app/models/faceted_search/facets/full_tree.rb
+++ app/models/faceted_search/facets/full_tree.rb
@@ -1,7 +1,7 @@
 module FacetedSearch
   class Facets::FullTree < Facets::DefaultList
     def values_with_parent(parent_id)
-      values.where(parent_id: parent_id)
+      values.select { |v| v.parent_id ==  parent_id }
     end
   end
 end
--- lib/faceted_search/version.rb
+++ lib/faceted_search/version.rb
@@ -1,3 +1,3 @@
 module FacetedSearch
-  VERSION = '3.1.0'
+  VERSION = '3.1.1'
 end
