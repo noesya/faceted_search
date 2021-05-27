fe66bb4c916db84b62c57dad8f5154d4fece343f
Arnaud Levy <contact@arnaudlevy.com>
2021-05-27 12:11:59 +0200

    filter default list with results

--- app/models/faceted_search/facets/default_list.rb
+++ app/models/faceted_search/facets/default_list.rb
@@ -28,7 +28,7 @@ module FacetedSearch
     def values
       unless @values
         joined_table = facets.model_table_name.to_sym
-        @values = source.all.joins(joined_table).where(joined_table => { id: facets.model }).distinct
+        @values = source.all.joins(joined_table).where(joined_table => { id: facets.results }).distinct
       end
       @values
     end
